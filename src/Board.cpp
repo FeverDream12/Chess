fb2e1179861af5b0d520f98c9d3805c899cef2f4
Slava <moiseev12348@gmail.com>
2020-04-26 22:05:19 +0800

    fix formatting in Board.cpp

--- src/Board.cpp
+++ src/Board.cpp
@@ -5,10 +5,8 @@ using namespace std;
 
 void BoardDraw(char board[][9])
 {
-    for (int i = 0; i < 9; i++)
-    {
-        for (int j = 0; j < 9; j++)
-        {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
             cout << board[i][j] << " ";
         }
         cout << endl;
