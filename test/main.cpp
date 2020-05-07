cdc61566c3c7ad964635c2343cd04f407d820733
Slava <moiseev12348@gmail.com>
2020-05-07 21:32:28 +0800

    Edit formating at main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -5,25 +5,25 @@
 using namespace std;
 
 char board[9][9] = {{'1', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-                   {'2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                   {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                   {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                   {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                   {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                   {'7', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                   {'8', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-                   {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
+                    {'2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                    {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {'7', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                    {'8', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+                    {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
 
 TEST_CASE("CorrectPawnMove", "Correct")
 {
     int Qualifier = 0;
     char playerMove[6] = "e2-e4";
-    REQUIRE(CorrectInput( playerMove,board,Qualifier) == true);
+    REQUIRE(CorrectInput(playerMove, board, Qualifier) == true);
 }
 
 TEST_CASE("IncorrectPawnMove", "Incorrect")
 {
     int Qualifier = 0;
     char playerMove[6] = "c8-c9";
-    REQUIRE(CorrectInput( playerMove,board,Qualifier) == false);
+    REQUIRE(CorrectInput(playerMove, board, Qualifier) == false);
 }
