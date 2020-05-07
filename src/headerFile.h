de15d7e67432d400a2fa767e7c0b6a8011ea036a
Slava <moiseev12348@gmail.com>
2020-05-07 14:47:29 +0800

    Add function to all figures moves

--- src/headerFile.h
+++ src/headerFile.h
@@ -3,5 +3,6 @@
 void PawnMove(char board[][9]);
 void BoardDraw(char board[][9]);
 void move(char playerMove[], char board[][9]);
-bool CorrectInput(char playerMove[], char board[][9]);
+void moveFigure(char playerMove[], char board[][9]);
+bool CorrectInput(char playerMove[], char board[][9], int Qualifier);
 #endif
