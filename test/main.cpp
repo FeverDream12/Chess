f655671f3490a8281c349c18e4019dd0296e8cea
Slava <moiseev12348@gmail.com>
2020-05-07 15:55:31 +0800

    Add new test case

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,4 @@
-#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this
-                          // in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "../src/headerFile.h"
 #include "../thirdparty/catch.hpp"
 #include <string>
@@ -15,9 +14,16 @@ char board[9][9] = {{'1', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
                    {'8', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
                    {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
 
-TEST_CASE("Move_Pawn", "Correct")
+TEST_CASE("CorrectPawnMove", "Correct")
 {
     int Qualifier = 0;
     char playerMove[6] = "e2-e4";
     REQUIRE(CorrectInput( playerMove,board,Qualifier) == true);
+}
+
+TEST_CASE("IncorrectPawnMove", "Incorrect")
+{
+    int Qualifier = 0;
+    char playerMove[6] = "c8-c9";
+    REQUIRE(CorrectInput( playerMove,board,Qualifier) == false);
 }
